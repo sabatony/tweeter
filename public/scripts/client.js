9a652b05064e9d2ade7a087a9b606b97b9b14742
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-08-25 17:40:24 -0700

    add validation errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const createTweetElement = function (tweet) {
-  let $tweet = `
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  let safeHTML = `
   <section class="tweet">
         <article>
           <header class="userInfo">
             <div id="avatar">
-            <img src=${tweet.user.avatars}>
-            <h3>${tweet.user.name}</h3>
+            <img src=${escape(tweet.user.avatars)}>
+            <h3>${escape(tweet.user.name)}</h3>
             </div>
-            <h4 id="username">${tweet.user.handle}</h4>
+            <h4 id="username">${escape(tweet.user.handle)}</h4>
           </header>
           <main>
             <p>
-              ${tweet.content.text}
+              ${escape(tweet.content.text)}
             </p>
           </main>
           <footer id="tweet-foot">
             <div class="foot-content">
-            <p>${tweet.created_at}</p>
+            <p>${timeago.format(tweet.created_at)}</p>
             <div id="icons">
               <i class="fa-solid fa-flag"></i>
               <i class="fa-solid fa-repeat"></i>
@@ -60,23 +44,56 @@ const createTweetElement = function (tweet) {
         </article>
       </section>`
       ;
-      return $tweet
+      return safeHTML
 };
 
-const renderTweets = function (tweets) {
-  for(const tweet of tweets) {
-    const tweetElement = createTweetElement(tweet);
-    $('#tweets-container').prepend(tweetElement);
-  }
-};
+
 
 
 
 $(document).ready(function(){
-  renderTweets(data)
+
+   const loadTweets = function () {
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:8080/tweets',
+  })
+  .then(function(tweet) {
+    renderTweets(tweet)
+  })
+ }
+ 
+
+  loadTweets();
+
+  const renderTweets = function (tweets) {
+    for(const tweet of tweets) {
+      const tweetElement = createTweetElement(tweet);
+      $('#tweets-container').prepend(tweetElement);
+    }
+  };
   
 $('form').submit(function(event) {
   event.preventDefault();
+  const tweetData = event.target[0].value
+  
+  if(tweetData.length > 140) {
+     $('#long-tweet').slideDown(1000, function() {
+      setTimeout(() => {
+        $('#long-tweet').hide();
+      },2000)
+    });
+    return
+  }
+
+  if(tweetData === '') {
+    $('#empty-tweet').slideDown(1000, function() {
+      setTimeout(() => {
+        $('#empty-tweet').hide();
+      },2000)
+    });
+    return
+  }
   console.log('data being sent')
   
   $.ajax({
@@ -84,6 +101,9 @@ $('form').submit(function(event) {
     url: 'http://localhost:8080/tweets',
     data: $(this).serialize()
 
+  }).then(() => {
+    $('#tweets-container').empty();
+    loadTweets();
   })
  })
 });
